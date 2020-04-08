b080c7bd080f224318d0401536002407a97cdb1d
0323sz <971955596@qq.com>
2020-04-09 04:01:05 +0800

    添加了一个输出内容

--- src/main/java/cn/houpu/gitdemo/test/GitDemo01.java
+++ src/main/java/cn/houpu/gitdemo/test/GitDemo01.java
@@ -3,5 +3,6 @@ package cn.houpu.gitdemo.test;
 public class GitDemo01 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello javagit");
     }
 }
